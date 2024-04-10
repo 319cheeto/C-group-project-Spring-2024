8e8fc05ee5160e639671eec00a973fd7e04e522d
Joshua W. Sears <joshuaw.sears@gmail.com>
2024-04-10 03:58:53 -0500

    working button.

--- bro-form.aspx.cs
+++ bro-form.aspx.cs
@@ -15,5 +15,23 @@ namespace bro_code
 
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            string name = TextBox1.Text;
+            string email = TextBox2.Text;
+            string message = TextBox3.Text;
+
+            if (name == "" || email == "" || message == "")
+            {
+                Label1.Text = "Please Add all needed information.";
+            }
+            else
+            {
+                Label1.Text = "Thank you for your message " + name + " we will get back to you soon";
+            }
+        }
     }
 }
